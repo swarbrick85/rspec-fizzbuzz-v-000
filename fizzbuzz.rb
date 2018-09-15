10ce528e0224fa1be3f41f36bb7c1ee3fc673e41
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-15 09:23:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(x)
   y = x % 3
   z = x % 5
-  if y == 0 && z == 0
+  if y == 0 && z !== 0
     puts "Fizz"
-  elsif z == false && b == true
+  elsif y !== 0 && z == 0
     puts "Buzz"
-  elsif z == true && b == true
+  elsif y == 0 && z == 0
     puts "FizzBuzz"
   end
 end
